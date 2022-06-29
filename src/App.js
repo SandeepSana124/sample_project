9664dfe34c79be5a0e7c14f8a276da27f94da890
singamramya <ramya.singam@xcubelabs.com>
2022-06-29 19:03:16 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import './App.css';
+// import "./Coponents/Details.css"
 import {Route, Routes} from "react-router-dom"
 import Header from './Coponents/Header';
 import Home from './Coponents/Home';
 import Footer from './Coponents/Footer';
 
+import DetailsPage from './Coponents/DetailsPage';
+
+
 function App() {
   return (
     <div className="App">
-       <Header/>
+       {/* <Header/>
      <Routes>
       <Route path="/" element={<Home/>}></Route>
-     </Routes>
-    
-     
-        <Footer/>
-    
-     
+     </Routes> */}
+      <DetailsPage/>
+        {/* <Footer/> */}
     </div>
   );
 }
