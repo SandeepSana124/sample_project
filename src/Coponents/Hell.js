25b7a5ef97c809b0a9c7b54a2a1c4050c3f3ff9f
chaitanyaboddu <chaitanya.boddu@xcubelabs.com>
2022-06-30 13:05:18 +0530

    updated

--- src/Coponents/Hell.js
+++ src/Coponents/Hell.js
@@ -8,9 +8,9 @@ function Hell() {
     <div>
       <div className='contain'>
         <div className="heading">
-          {/* <div class="server-room-purple-uhd-4k-wallpaper"> */}
-          {/* <img src={require('file:///home/chaitanyaboddu/.cache/.fr-FMSbB6/server-room-purple-uhd-4-k-wallpaper.png')} alt='UFC Logo' className='' /> */}
-          <nav className="navbar navbar-expand-lg">
+
+
+<nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
               {/* <a className="navbar-brand" href="#"> */}
               <div>
@@ -30,14 +30,9 @@ function Hell() {
                   <li className="nav-item dropdown">
                     {/* <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> */}
                     <a className="nav-link" href="#">OUR APPROACH</a>
-                    {/* </a> */}
-                    {/* <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            {/* <li><a className="dropdown-divider"></li> */}
-                    {/* <li><a className="dropdown-item" href="#">Something else here</a></li> */}
-                    {/* </ul>  */}
-                  </li>
+                    
+                    
+                     </li>
                   <li className="nav-item">
                     <a className="nav-link" href="#">ABOUT US</a>
                   </li>
@@ -52,7 +47,7 @@ function Hell() {
                   </li>
 
 
-                </ul>
+                   </ul>
                 {/* <form className="d-flex"> */}
                 <div className="d-flex">
                   <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -82,9 +77,9 @@ function Hell() {
         </div>
       </div>
 
-   
-     
-      );
+
+
+);
 }
 
       export default Hell;
