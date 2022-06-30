66d01f9c5e1d13d30a5aab213d53ed5499287cb3
Chandrikabogem <chandrika.bogem@xcubelabs.com>
2022-06-30 13:07:01 +0530

    Merge branch 'main' of https://github.com/SandeepSana124/sample_project into chandubranch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import './App.css';
-import "./Assests/Home.css"
+/* import './App.css';
+import "./Assests/Home.css" */
 import {Route, Routes} from "react-router-dom"
-import Header from './Coponents/Header';
+/* import Header from './Coponents/Header';
 import Home from './Coponents/Home';
-import Footer from './Coponents/Footer';
+import Footer from './Coponents/Footer'; */
+import Hello from "./Coponents/Hello";
+
+import ChooseProvide from './Coponents/ChooseProvider'
+import Setupmanual from './Coponents/SetupManual'
+
 
 function App() {
   return (
     <div className="">
-       <Header/>
+      {/*  <Header/>
      <Routes>
       <Route path="/" element={<Home/>}></Route>
      </Routes>
     
      
-        <Footer/>
-    
+        <Footer/> */}
+    <Hello/>
+     <ChooseProvide/> 
+  {/*   <Setupmanual/> */}
      
     </div>
   );
---
+++ src/Coponents/SetupManual.js
@@ -0,0 +1,128 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Setupmanual.css'
+
+const SetupManual = () => {
+    return (
+        <div>
+            <nav class="navbar navbar-light bg-light mx-5">
+                <div class="container-fluid">
+                    <a class="navbar-brand display" href="#">
+
+                        <div alt="" width="30" height="24" class="d-inline-block align-text-top iconcontainer px-4" />
+                        <div>
+                            <p className='spot px-2'>SpotWorks</p>
+                            <p className='spot2 px-1'>Powered by [X]Cube LABS</p>
+                        </div>
+                    </a>
+
+
+
+                    <div class="shape justify-content-end"></div>
+                </div>
+
+
+            </nav>
+
+
+
+
+
+
+            <div className='container chooseicon m-10'>
+                <div className='container Rectangle mx-5 py-5 my-5'>
+                    <div className='spotcon'>
+                        <div className='cont'>
+                            <p className='spot'>SpotWorks</p>
+                            <p className='spot2'>Powered by [X]Cube LABS</p>
+
+                        </div>
+                        <div className='py-5'>
+                            <h5>Connect an AWS Account to Optimize its Costs</h5>
+                            <p >
+
+                                There’s no need to worry, SpotWorks will never modify any of your existing workloads
+                            </p>
+
+
+                        </div>
+                        <div className='display3 mx-5 px-5 spotcon'>
+                            <div className='spotcon'>
+                                <button className='btn manual px-5'>Manual Connection
+
+                                </button>
+                            </div>
+                            <div>
+                                <button className='btn connection px-5'>SpotWorks Connections</button>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <div className='display'>
+                        <div>
+                            <button className='btn manual'>Manual Connection
+
+                            </button>
+                        </div>
+                        <div>
+                            <button className='btn connection'>SpotWorks Connections</button>
+                        </div>
+                    </div> */}
+
+                    <div className='question'>
+                        <h5 className='head py-3'>STEP1</h5>
+                        <p className='content2 py-1'>Are you connecting to SpotWorks with an AWS China account ?</p>
+                        <input className='mx-3' type="radio" />Yes
+                        <input className='mx-3' type="radio" />No
+                        <p className='content2 py-2' >Are you connecting to SpotWorks with an AWS GovCloud account ?</p>
+                        <input className='mx-3' type="radio" />Yes
+                        <input className='mx-3' type="radio" />No
+                        <h5 className='head py-2'>STEP2</h5>
+                        <div className='display2 py-1'>
+                            <p className='content2 '>Open the IAM console.</p>
+                            <button className='btn connection button mx-5 px-5'>OPEN</button>
+
+                        </div>
+                        <h5 className='head'>STEP3</h5>
+                        <div className='display2 py-2'>
+                            <p className='content2'>Click the Policies tab on the sidebar, and select 'Create Policy'.<br />
+                                Switch to JSON tab.<br />
+                                Paste the attached policy and click 'Review Policy'.<br />
+                                Set the policy name to Spot-Policy and click 'Create Policy'.</p>
+                            <button className='btn connection button2 px-4 mx-5'>View Policy</button>
+                        </div>
+                        <h5 className='head py-1'>STEP4</h5>
+                        <p className='content2'>Click the Roles tab on the sidebar, and click 'Create Role'.<br />
+                            Select another AWS account and enter Account ID: 922761411349</p>
+                        <h5 className='head'>STEP5</h5>
+                        <p className='content2'>Click the Require external ID checkbox and enter the External ID:spotinst:aws:extid:o527bf22o0000000</p>
+                        <p className='content2'>When you are finished, click 'Next'.</p>
+                        <h5 className='head'>STEP6</h5>
+                        <div className='display2 py-2'>
+                            <p className='content2'>Set role name: SpotWorks-Role, Copy the RoleArn from <br />
+                                the role’s screen and paste it here:</p>
+                            <button className='btn connection button3 '>Email Id</button>
+                        </div>
+                        <div className='display py-3'>
+                            <div>
+                                <button className='btn manual btn1 mx-4'>BACK
+
+                                </button>
+                            </div>
+                            <div>
+                                <button className='btn btn-primary btn2 connection mx-1'>CONNECT</button>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+
+
+
+            </div>
+
+        </div>
+    )
+}
+
+export default SetupManual
