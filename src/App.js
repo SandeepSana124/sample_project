d7aa988bf53a2458d3c6050ea8698af6ab9ebf8f
manoj12529 <manoj.gundeti@xcubelabs.com>
2022-06-29 23:05:13 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -7,28 +7,16 @@ import {Route, Routes} from "react-router-dom"
 import Header from './Coponents/Header';
 import Home1 from './Coponents/Home1';
 import Footer from './Coponents/Footer';
-<<<<<<< HEAD
 import Hell from './Coponents/Hell';
-=======
 import Home from './Coponents/Home';
 
 
 
 
->>>>>>> a499a097593c6636e2944dc4194e8a1b7b2daa78
 
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      {/*  <Header/> */}
-   {/*   <Routes>
-      <Route path="/" element={<Home/>}></Route>
-     </Routes> */}
-    
-     <Hell/>
-=======
     <div className="">
        <Header/>
      <Routes>
@@ -36,7 +24,6 @@ function App() {
      </Routes>
      <Home1/>
 
->>>>>>> a499a097593c6636e2944dc4194e8a1b7b2daa78
      
        {/*  <Footer/> */}
     
