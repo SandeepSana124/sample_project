7a5c556156b839dde249c6a366bb9d48ffe88628
SandeepSana124 <sandeep.sana@xcubelabs.com>
2022-07-01 10:50:43 +0530

    ss

--- src/Coponents/Hell.js
+++ src/Coponents/Hell.js
@@ -8,13 +8,13 @@ function Hell() {
     <div>
       <div className='contain'>
         <div className="heading">
-          {/* <div class="server-room-purple-uhd-4k-wallpaper"> */}
-          {/* <img src={require('file:///home/chaitanyaboddu/.cache/.fr-FMSbB6/server-room-purple-uhd-4-k-wallpaper.png')} alt='UFC Logo' className='' /> */}
-          <nav className="navbar navbar-expand-lg">
+
+
+<nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
               {/* <a className="navbar-brand" href="#"> */}
               <div>
-                <img src={require("/home/sandeepsana/Desktop/Routing/sample_project/src/Assests/footer1.png")} alt='UFC Logo' className='spotwork' />
+                <img src={require("/home/sandeepsana/Desktop/Routing/sample_project/src/Assests/group.png")} alt='UFC Logo' className='spotwork' />
               </div>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
@@ -22,7 +22,7 @@ function Hell() {
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                   <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">SOLUTIONS</a>
+                    <a className="nav-link" href="#">SOLUTIONS</a>
                   </li>
                   <li className="nav-item">
                     <a className="nav-link" href="#">RESOURCES</a>
@@ -30,14 +30,9 @@ function Hell() {
                   <li className="nav-item dropdown">
                     {/* <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> */}
                     <a className="nav-link" href="#">OUR APPROACH</a>
-                    {/* </a> */}
-                    {/* <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            {/* <li><a className="dropdown-divider"></li> */}
-                    {/* <li><a className="dropdown-item" href="#">Something else here</a></li> */}
-                    {/* </ul>  */}
-                  </li>
+                    
+                    
+                     </li>
                   <li className="nav-item">
                     <a className="nav-link" href="#">ABOUT US</a>
                   </li>
@@ -52,7 +47,7 @@ function Hell() {
                   </li>
 
 
-                </ul>
+                   </ul>
                 {/* <form className="d-flex"> */}
                 <div className="d-flex">
                   <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -82,9 +77,9 @@ function Hell() {
         </div>
       </div>
 
-   
-     
-      );
+
+
+);
 }
 
       export default Hell;
